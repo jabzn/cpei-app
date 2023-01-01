697c99dc5137eb2659a709428cadf11546c42033
Jabal Noer <jabalnoer12@gmail.com>
2023-01-01 23:17:14 +0800

    Add Layout

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,63 @@
+<script setup>
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <header>
+        <!--Nav-->
+        <nav aria-label="menu nav" class="bg-gray-800 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
+            <div class="flex flex-wrap items-center justify-between mr-12 ml-4">
+                <div class="flex flex-shrink md:w-1/3 justify-between md:justify-start text-white">
+                    <a href="#" aria-label="Home">
+                        <Link :href="route('dashboard')">
+                            <ApplicationLogo
+                                class="block h-9 w-auto fill-current text-blue-800"
+                            />
+                        </Link>
+                    </a>
+                </div>
+
+                <div class="sm:flex sm:items-center sm:ml-6 pt-2">
+                    <!-- Settings Dropdown -->
+                    <div class="ml-3 relative">
+                        <Dropdown align="right" width="48">
+                            <template #trigger>
+                                <span class="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-md leading-4 font-medium rounded-md text-gray-200 hover:text-white focus:outline-none transition ease-in-out duration-150"
+                                    >
+                                        {{ $page.props.auth.user.name }}
+
+                                        <svg
+                                            class="ml-2 -mr-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </template>
+
+                            <template #content>
+                                <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                <DropdownLink :href="route('logout')" method="post" as="button">
+                                    Log Out
+                                </DropdownLink>
+                            </template>
+                        </Dropdown>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Navbar from '@/Components/Navbar.vue';
+import Sidebar from '@/Components/Sidebar.vue';
+</script>
+
+<template>
+    <Navbar />
+
+    <div class="flex flex-col md:flex-row">
+        <Sidebar />
+
+        <section class="w-full">
+            <div class="main-content flex-1 mt-12 md:mt-2 pb-24 md:pb-5">
+                <div class="bg-gray-800 pt-3 mt-12">
+                    <header class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white" v-if="$slots.header">
+                        <h1 class="font-bold pl-2">
+                            <slot name="header" />
+                        </h1>
+                    </header>
+                </div>
+
+                <main>
+                    <div class="py-4">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div class="p-6 text-gray-900">
+                                    <slot name="content" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </section>
+    </div>
+</template>
